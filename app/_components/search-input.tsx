3218e013c29bf340f5b42c5ec1733c29dcbc17e2
JoaoRajiv <olivairarajiv@gmail.com>
2025-11-11 16:38:34 -0300

    First Commit

---
+++ app/_components/search-input.tsx
@@ -0,0 +1,15 @@
+import { SearchIcon } from "lucide-react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const SearchInput = () => {
+    return (
+        <div className="flex items-center gap-2">
+            <Input type="text" placeholder="Pesquise serviços ou barbearia" className="border-border rounded-full"/>
+            <Button variant="default" size="icon" className="rounded-full">
+                <SearchIcon />
+            </Button>
+        </div>
+    );
+};
+export default SearchInput;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Header from "./_components/header";
+import SearchInput from "./_components/search-input";
+import banner from "../public/banner.png";
 
 export default function Home() {
   return (
     <div>
-      
+      <Header />
+      <div className="px-5 space-y-4">
+      <SearchInput />
+      <Image src={banner} alt="Aparatus Banner" className="rounded-lg h-auto w-full" sizes="100vw"/>
+      </div>
     </div>
   );
 }
